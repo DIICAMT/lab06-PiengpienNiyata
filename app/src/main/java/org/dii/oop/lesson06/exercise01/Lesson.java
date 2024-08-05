5f7ab67b44a640f45f843f880397fd23cdc6ccf6
PiengpienNiyata <151113312+PiengpienNiyata@users.noreply.github.com>
2024-08-05 14:45:45 +0700

    .

--- app/src/main/java/org/dii/oop/lesson06/exercise01/Lesson.java
+++ app/src/main/java/org/dii/oop/lesson06/exercise01/Lesson.java
@@ -1,34 +1,36 @@
-package app.src.main.java.org.dii.oop.lesson06.exercise01;
+package org.dii.oop.lesson06.exercise01;
 
 import java.util.Scanner;
 import java.util.ArrayList; //this exercise MUST use arrayList
 
 public class Lesson {
-    public static void run() {
 
+    public static void run() {
+        ArrayList<Shape> shapes = new ArrayList<Shape>();
         //HINT1: usage arraylist with Shape class to store all of your shapes, check how to use arrayList by yourself
 
         //HINT2: you may want to declare your arrayList around here
 
-        ArrayList<org.dii.oop.Shape> shapes = new ArrayList<>(); // ArrayList to store shapes
         Scanner in = new Scanner(System.in);
-
         do {
             System.out.println();
-            System.out.println("1. Create Circle");
-            System.out.println("2. Create Rectangle");
-            System.out.println("3. Create Square");
-            System.out.println("4. List all shapes");
-            System.out.println("5. Exit");
-            System.out.print("Please select [1-5]: ");
-            String choice = in.nextLine().trim();
+            //print menu as instructed in MD file
+            System.out.println("""
+                    1. Circle
+                    2. Rectangle
+                    3. Square
+                    4. Display all shape
+                    5. exit""");
 
+            String choice = in.nextLine().trim();
 
             // TODO: write your code here
 
             if ("1".equals(choice)) {
                 System.out.print("Enter radius: ");
                 String strRadius = in.nextLine();
+                shapes.add(new Circle("Circle",strRadius));
+
                 
                 //store to arrayList
             }
@@ -37,24 +39,31 @@ public class Lesson {
                 String strWidth = in.nextLine();
                 System.out.print("Enter height: ");
                 String strHeight = in.nextLine();
+                shapes.add(new Rectangle("Rectangle",strWidth,strHeight));
+
                 
                 //store to arrayList
             }
             if ("3".equals(choice)) {
                 System.out.print("Enter side: ");
                 String strSide = in.nextLine();
-                
+                shapes.add(new Square("Square",strSide));
                 //store to arrayList
             }
             if ("4".equals(choice)) {
-                System.out.print("List all shape: ");
-                
+                System.out.print("List all shape: \n");
+                for (Shape shape : shapes) {
+                    System.out.println(shape.getName() +" Number of side: " + shape.getNumSides() +" Area: " + shape.getArea() + " Perimeter: " +shape.getPerimeter());
+                }
                 //print data as instructed
                 //check how to iterate through arrayList
 
                 //HINT3: you can use getClass() function of arrayList to get the name of class. It might be useful : )
             }
-            if ("5".equals(choice)) {
+            if("5".equals(choice)){
+                break;
+            }
+            if ("6".equals(choice)) {
                  break;
             }
            
